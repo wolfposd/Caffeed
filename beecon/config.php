5b7f00eee07e63bb14b64300d99ff812d09a0ddf
Wolf Posdorfer <9posdorf@informatik.uni-hamburg.de>
2014-04-01 12:19:28 +0200

    init

---
+++ beecon/config.php
@@ -0,0 +1,13 @@
+<?php
+
+
+
+$db_host = "localhost";
+$db_database = "beecon";
+$db_username ="root";
+$db_password ="";
+
+$mysqli = mysqli_connect($db_host, $db_username, $db_password, $db_database);
+
+
+?>
