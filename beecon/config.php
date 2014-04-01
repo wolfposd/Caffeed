8cbb02655fd00711a3433e2328f3ba9d2518bdca
Wolf Posdorfer <9posdorf@informatik.uni-hamburg.de>
2014-04-01 14:00:11 +0200

    as class

--- beecon/config.php
+++ beecon/config.php
@@ -1,13 +1,13 @@
 <?php
 
 
-
-$db_host = "localhost";
-$db_database = "beecon";
-$db_username ="root";
-$db_password ="";
-
-$mysqli = mysqli_connect($db_host, $db_username, $db_password, $db_database);
-
+function getConnection()
+{
+	$db_host = "localhost";
+	$db_database = "beecon";
+	$db_username ="root";
+	$db_password ="";
+	return new mysqli($db_host, $db_username, $db_password, $db_database);
+}
 
 ?>
--- beecon/index.php
+++ beecon/index.php
@@ -1,7 +1,77 @@
 <?php
 
+include_once 'config.php';
 
 
+
+// var_dump($_SERVER["REQUEST_METHOD"]);
+// var_dump($_SERVER);
+
+
+
+// echo "<br><br><br>";
+$application = new Application();
+$application->handle();
+
+
+class Application {
+
+	public $mysqli;
+
+	function __construct()
+	{
+		$this->mysqli = getConnection();
+	}
+	function __destruct()
+	{
+		$this->mysqli->close();
+	}
+
+	function handle()
+	{
+		$request = explode("/", substr(@$_SERVER['PATH_INFO'], 1));
+		$rest = 'rest_'.strtolower($_SERVER['REQUEST_METHOD']);
+		
+		if (method_exists($this,$rest)) 
+		{
+			call_user_func(array($this, $rest), $request);
+		}
+	}
+	
+	
+	function rest_get($request)
+	{
+		$function = "rest_get_".$request[0];
+
+		if(method_exists($this,$function))
+		{
+			call_user_func(array($this,$function),$request[1]);
+		}
+	}
+
+
+	function rest_get_beaconsbyuuid($uuid)
+	{
+		$uuid = $this->mysqli->real_escape_string($uuid);
+
+		$query = "SELECT * FROM ibeacons WHERE UUID = '$uuid'";
+
+		$result = $this->mysqli->query($query,MYSQLI_USE_RESULT);
+
+		$returnval = array();
+
+		while($row = $result->fetch_assoc())
+		{
+			unset($row["id"]);
+			array_push($returnval, $row);
+		}
+
+		$result->close();
+
+		echo json_encode($returnval, JSON_FORCE_OBJECT);
+	}
+
+}
 
 
 
