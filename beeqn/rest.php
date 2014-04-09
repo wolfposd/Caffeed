d83d1a04c2c161a0a121fc877c7390f45d7806d5
Wolf Posdorfer <9posdorf@informatik.uni-hamburg.de>
2014-04-09 15:23:23 +0200

    rest update

--- beeqn/rest.php
+++ beeqn/rest.php
@@ -24,8 +24,8 @@ class Application {
 	{
 		$request = explode("/", substr(@$_SERVER['PATH_INFO'], 1));
 		$rest = 'rest_'.strtolower($_SERVER['REQUEST_METHOD'])."_".$request[0];
-		
-		if (method_exists($this,$rest)) 
+
+		if (method_exists($this,$rest))
 		{
 			call_user_func(array($this, $rest), $request);
 		}
@@ -33,47 +33,68 @@ class Application {
 
 	function rest_get_beaconsinfo($arrayvalue)
 	{
-		
+
 		$uuid = $this->mysqli->real_escape_string($arrayvalue[1]);
 
 		$query = "SELECT * FROM ibeacons, ibeacon_actions WHERE ibeacons.id = ibeacon_actions.id AND UUID = '$uuid'";
-		
-		
+
+
 		if(in_array("major", $arrayvalue))
 		{
 			$index = array_search("major", $arrayvalue);
-			$major = $this->mysqli->real_escape_string($arrayvalue[$index+1]);
-			$query .= " AND major = $major ";
+			if($index + 1 < count($arrayvalue))
+			{
+				$major = $this->mysqli->real_escape_string($arrayvalue[$index+1]);
+				
+				echo "WHAT -$major-";
+				
+				if(int_ok($major))
+				{
+					$query .= " AND major = $major ";
+				}
+			}
 		}
 		if(in_array("minor", $arrayvalue))
 		{
 			$index = array_search("minor", $arrayvalue);
-			$major = $this->mysqli->real_escape_string($arrayvalue[$index+1]);
-			$query .= " AND minor = $major ";
+			if($index + 1 < count($arrayvalue))
+			{
+				$minor = $this->mysqli->real_escape_string($arrayvalue[$index+1]);
+				if(int_ok($minor))
+				{
+					$query .= " AND minor = $minor ";
+				}
+			}
 		}
-		
-		$query .= " LIMIT 1";
 
-		$result = $this->mysqli->query($query,MYSQLI_USE_RESULT);
+		$query .= " LIMIT 1";
+		
+		$result = $this->mysqli->query($query, MYSQLI_USE_RESULT);
 
 		$returnval = array();
 
 		while($row = $result->fetch_assoc())
 		{
-			unset($row["id"]);
-			$returnval = $row;
+			$returnval["action_type"] = $row["action_type"];
+			$returnval["action_info"] = $row["action_info"];
+			break;
 		}
 
 		$result->close();
 
 		if(count($returnval) == 0)
-		{	
+		{
 			array_push($returnval, array("error"=>"noinfo"));
 		}
 
 		echo json_encode($returnval);
 	}
 
+}
+
+function int_ok($val)
+{
+	return ($val !== true) && ((string)(int) $val) === ((string) $val);
 }
 
 
