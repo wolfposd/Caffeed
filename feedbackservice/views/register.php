c4d7d611bd4396b969cdcfffe2f891ea01de42ca
Wolf Posdorfer <9posdorf@informatik.uni-hamburg.de>
2014-05-14 16:32:35 +0200

    register page javascript checks

--- feedbackservice/views/register.php
+++ feedbackservice/views/register.php
@@ -51,7 +51,27 @@ class Register
 	 */
 	function additionalJavascript()
 	{
-		return ""; // additional javascript
+		return '$(function(){
+			$("#register").click(function(){
+				$(".error").hide();
+				var passwordVal = $("#pwd").val();
+				var checkVal = $("#pwd_again").val();
+				if (passwordVal == "") {
+					$("#errorfield").html("Please enter a password!");
+		            $("#pwd").focus();
+					return false;
+				} else if (checkVal == "") {
+					$("#errorfield").html("Please retype your password!");
+		            $("#pwd_again").focus();
+					return false;
+				} else if (passwordVal != checkVal ) {
+					$("#errorfield").html("The passwords do not match");
+		            $("#pwd_again").focus();
+					return false;
+				}
+				return true;
+			});
+		});'; // additional javascript
 	}
 
 }
@@ -62,7 +82,7 @@ function show_register_form()
 <div class="col-md-4 col-lg-4" style="float: none; margin: 0 auto;">
 	<div id="message"></div>
 	<form class="form-signin" method="post">
-		<h2 class="form-signin-heading">Please sign in</h2>
+		<h2 class="form-signin-heading text-center">Please fill out the registration form</h2>
 		<input type="email" class="form-control" placeholder="Email address" name="user" id="user" required autofocus>
 		<p></p>
 		<input type="text" class="form-control" placeholder="First name" name="firstname" id="firstname" required>
@@ -74,6 +94,7 @@ function show_register_form()
 		<input type="password" class="form-control"	placeholder="Repeat Password" name="pwd_again" id="pwd_again" required>
 		<p></p>
 		<button class="btn btn-lg btn-primary btn-block" type="submit" id="register">Register</button>
+		<div class="text-center"><span id="errorfield"  style="color:#FF0000;font-size:200%"></span></div>
 	</form>
 </div>
 <?php 
