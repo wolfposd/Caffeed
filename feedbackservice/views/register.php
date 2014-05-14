9733fe78c179db7e07a7bc20caf91f38f1f28e94
Wolf Posdorfer <9posdorf@informatik.uni-hamburg.de>
2014-05-14 16:18:35 +0200

    register layout

--- feedbackservice/views/register.php
+++ feedbackservice/views/register.php
@@ -6,6 +6,8 @@
 class Register
 {
 	private $mysqli;
+	
+	private $mode = 0;
 
 	function __construct(&$mysqli)
 	{
@@ -15,9 +17,31 @@ class Register
 	{
 	}
 
+	
+	function setup()
+	{
+	    if(isset($_POST["pwd"]))
+	    {
+	        $this->mode = 1;
+	    }
+	    else
+	    {
+	        
+	    }
+	}
+	
 	function display()
 	{
-		echo "This is the Registration";
+	    switch($this->mode)
+	    {
+	        case 1:
+	            {
+	                echo "user has pressed button";
+	                break;
+	            }
+	        default:
+	            show_register_form();
+	    }
 	}
 
 
@@ -30,6 +54,29 @@ class Register
 		return ""; // additional javascript
 	}
 
+}
+
+function show_register_form()
+{
+    ?>
+<div class="col-md-4 col-lg-4" style="float: none; margin: 0 auto;">
+	<div id="message"></div>
+	<form class="form-signin" method="post">
+		<h2 class="form-signin-heading">Please sign in</h2>
+		<input type="email" class="form-control" placeholder="Email address" name="user" id="user" required autofocus>
+		<p></p>
+		<input type="text" class="form-control" placeholder="First name" name="firstname" id="firstname" required>
+		<p></p>
+		<input type="text" class="form-control" placeholder="Last name" name="lastname" id="lastname" required >
+		<p></p>
+		<input type="password" class="form-control" placeholder="Password" name="pwd" id="pwd" required>
+		<p></p>
+		<input type="password" class="form-control"	placeholder="Repeat Password" name="pwd_again" id="pwd_again" required>
+		<p></p>
+		<button class="btn btn-lg btn-primary btn-block" type="submit" id="register">Register</button>
+	</form>
+</div>
+<?php 
 }
 
 
