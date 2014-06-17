60e0140fc0278c4d736727a1a55be4886d2e57aa
Wolf Posdorfer <9posdorf@informatik.uni-hamburg.de>
2014-06-17 14:03:11 +0200

    horizontal line optimization

--- feedbackservice/views/modules/pagebreak.module.php
+++ feedbackservice/views/modules/pagebreak.module.php
@@ -30,10 +30,16 @@ class pagebreak implements IModule
     
     function editorhtml()
     {
+        $hline ="";
+        for($i=0; $i <= 20;$i++)
+        {
+            $hline.="&#8211;";
+        }
+        
         ?>
             <div class="row" id="div_module_XXXX">
                 <h4>
-                    <font color="red">--------------------------- Pagebreak ---------------------------</font>
+                    <font color="red"><?php echo $hline?> Pagebreak <?php echo $hline?></font>
                     <button type="button" class="btn btn-danger" onclick="$('#div_module_XXXX').remove(); return false;">X</button>
                 </h4>
                 <input type="hidden" name="module_XXXX_type" value="pagebreak">
