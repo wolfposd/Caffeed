cfde92975e8b86be2c7aed7b04ce8d438af40751
Wolf Posdorfer <9posdorf@informatik.uni-hamburg.de>
2014-06-24 17:12:58 +0200

    no keyboard on date

--- feedbackservice/views/modules/date.module.php
+++ feedbackservice/views/modules/date.module.php
@@ -7,7 +7,7 @@ class date extends AbstractModule
     
     public function javascript()
     {
-        return 'var dp'.$this->id.'= $("#'.$this->id.'").datepicker({format:"dd.mm.yyyy"}).on("changeDate", function(ev) { dp'.$this->id.'.hide(); }).data("datepicker");';
+        return 'var dp'.$this->id.'= $("#'.$this->id.'").datepicker({format:"dd.mm.yyyy",weekStart:1}).on("changeDate", function(ev) { dp'.$this->id.'.hide(); }).data("datepicker");';
     }
 
     public function html()
@@ -16,7 +16,7 @@ class date extends AbstractModule
         <div class="text-center">
             <p></p>
     	    <p><?php echo $this->values["text"]?><p>
-    	    <input type="text" class="span2 text-center" value="<?php echo date("d.m.Y");?>" name="<?php echo $this->id?>" id="<?php echo $this->id?>">
+    	    <input type="text" class="span2 text-center" value="<?php echo date("d.m.Y");?>" name="<?php echo $this->id?>" id="<?php echo $this->id?>" readonly="true">
         </div>
         <?php 
     }
